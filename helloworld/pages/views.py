6ddb66ee518e31d629f90939cebf2ce0b0a4b94f
Samuel <sherrerah3@eafit.edu.co>
2025-08-05 21:06:58 -0500

    Implementacion modelos django

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -1,9 +1,12 @@
 from django import forms
-from django.shortcuts import render, redirect
-from django.views.generic import TemplateView
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.generic import TemplateView, ListView
 from django.views import View
 from django.http import HttpResponseRedirect
 from django.urls import reverse
+from django import forms
+from django.core.exceptions import ValidationError
+from .models import Product
 
 
 # Create your views here.
@@ -38,14 +41,6 @@ class ContactPageView(TemplateView):
  
         return context 
     
-class Product:
-    products = [
-        {"id": "1", "name": "TV", "description": "Best TV", "price": 120},
-        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 999},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 80},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 30},
-    ]
-
 class ProductIndexView(View):
     template_name = 'products/index.html'
 
@@ -53,7 +48,7 @@ class ProductIndexView(View):
         viewData = {}
         viewData["title"] = "Products - Online Store"
         viewData["subtitle"] = "List of Products"
-        viewData["products"] = Product.products
+        viewData["products"] = Product.objects.all()
 
         return render(request, self.template_name, viewData)
 
@@ -62,19 +57,28 @@ class ProductShowView(View):
     template_name = 'products/show.html'
 
     def get(self, request, id):
-        if not id.isdigit() or int(id) < 1 or int(id) > len(Product.products):
+        # Check if product id is valid
+        try:
+            product_id = int(id)
+            if product_id < 1:
+                raise ValueError("Product id must be 1 or greater")
+            product = get_object_or_404(Product, pk=product_id)
+        except (ValueError, IndexError):
+            # If the product id is not valid, redirect to the home page
             return HttpResponseRedirect(reverse('home'))
 
         viewData = {}
-        product = Product.products[int(id)-1]
-        viewData["title"] = product["name"] + " - Online Store"
-        viewData["subtitle"] = product["name"] + " - Product information"
+        product = get_object_or_404(Product, pk=product_id)
+        viewData["title"] = product.name + " - Online Store"
+        viewData["subtitle"] = product.name + " - Product information"
         viewData["product"] = product
         return render(request, self.template_name, viewData)
 
-class ProductForm(forms.Form):
-    name = forms.CharField(required=True)
-    price = forms.FloatField(required=True)
+
+class ProductForm(forms.ModelForm):
+    class Meta:
+        model = Product
+        fields = ['name','price']
 
     def clean_price(self):
         price = self.cleaned_data.get('price')
@@ -96,5 +100,23 @@ class ProductCreateView(View):
     def post(self, request):
         form = ProductForm(request.POST)
         if form.is_valid():
-            return render(request, 'products/product_created.html', {'title': 'Product created'})
-        return render(request, self.template_name, {'form': form, 'title': 'Create product'})
+            form.save()
+            return redirect('/products/create')
+        else:
+            viewData = {}
+            viewData['title'] = "Create Product"
+            viewData['form'] = form
+            return render(request, self.template_name, viewData)
+
+
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products_list.html'
+    context_object_name = 'products' # This will allow u to loop through 'products' in your template
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Products - Online Store'
+        context['subtitle'] = 'List of Products'
+        return context
+    
