2d633030bb454201c2b94437e14d38ec2cb7cb5f
Samuel <sherrerah3@eafit.edu.co>
2025-08-12 23:33:09 -0500

    Implementacion DI

--- helloworld/pages/views.py
+++ helloworld/pages/views.py
@@ -7,6 +7,7 @@ from django.urls import reverse
 from django import forms
 from django.core.exceptions import ValidationError
 from .models import Product
+from .utils import ImageLocalStorage
 
 
 # Create your views here.
@@ -120,3 +121,75 @@ class ProductListView(ListView):
         context['subtitle'] = 'List of Products'
         return context
     
+class CartView(View):
+    template_name = 'cart/index.html'
+
+    def get(self, request):
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'TV Samsung', 'price':'1000'}
+        products[11] = {'name':'Iphone', 'price':'2000'}
+
+        # Get cart products from session
+        cart_products = {}
+        cart_products_data = request.session.get('cart_product_data',{})
+
+        for key, product in products.items():
+            if str(key) in cart_products_data.keys():
+                cart_products[key] = product
+
+        # Prepare data for the view
+        view_data = {
+            'title': 'Cart - Online Store', 
+            'subtitle': 'Shopping Cart', 
+            'products': products, 
+            'cart_products': cart_products  
+        }
+        
+        return render(request, self.template_name, view_data) 
+   
+    def post(self, request, product_id): 
+        # Get cart products from session and add the new product 
+        cart_product_data = request.session.get('cart_product_data', {}) 
+        cart_product_data[product_id] = product_id 
+        request.session['cart_product_data'] = cart_product_data 
+ 
+        return redirect('cart_index') 
+    
+class CartRemoveAllView(View): 
+    def post(self, request): 
+        # Remove all products from cart in session 
+        if 'cart_product_data' in request.session: 
+            del request.session['cart_product_data'] 
+ 
+        return redirect('cart_index')
+    
+def ImageViewFactory(image_storage):
+    class ImageView(View):
+        template_name = 'images/index.html'
+
+        def get(self, request): 
+            image_url = request.session.get('image_url', '') 
+            return render(request, self.template_name, {'image_url': image_url}) 
+ 
+        def post(self, request): 
+            image_url = image_storage.store(request) 
+            request.session['image_url'] = image_url 
+            return redirect('image_index') 
+
+    return ImageView
+
+class ImageViewNoDI(View):
+    template_name = 'images/index.html'
+
+    def get(self, request):
+        image_url = request.session.get('image_url', '')
+
+        return render(request, self.template_name, {'image_url': image_url})
+
+    def post(self, request):
+        image_storage = ImageLocalStorage()
+        image_url = image_storage.store(request)
+        request.session['image_url'] = image_url
+
+        return redirect('image_index')
