972db7fd4ad3fc7053257814dd2ca67118134555
sherrerah3 <sherrerah3@eafit.edu.co>
2025-09-30 14:03:24 -0500

    Arreglo en archivos asgi y wsgi

--- helloworld/helloworld_project/asgi.py
+++ helloworld/helloworld_project/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hellowolrd_project.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helloworld_project.settings')
 
 application = get_asgi_application()
